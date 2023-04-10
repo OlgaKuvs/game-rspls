52c240a578a4fe7123ca67db12c3168b38ada619
Olga Kuvsincikova <olja_kuv@inbox.lv>
2023-04-10 16:32:03 +0000

    Fix bugs in gameOver function

--- assets/js/script.js
+++ assets/js/script.js
@@ -194,112 +194,6 @@ function ShowHideSections() {
     scoreboard.style.display = "block";     
 }
 
-/*
-function playGame() {
-    
-    let items = document.getElementsByClassName("circular-menu-item");
-   
-    if (level === 1 && flagItems === 1)   {
-
-        for (let item of items) {
-            item.addEventListener("click", function() {
-            
-                console.log("ccc ", item);
-            
-                userChoice = item.id;
-
-                compChoice = computerChoice();
-
-                result = calculateResult(userChoice, compChoice);
-
-                whoWon = showResult(userChoice, compChoice); 
-                        
-                
-            });           
-        }  
-    
-        flagItems++;
-      
-        
-    }  else if (level === 2 && flagItems === 2)   {
-
-        userCounter = 0;
-        compCounter = 0;
-        userScore.innerText = userCounter; 
-        compScore.innerText = compCounter;  
-
-        console.log("RESET1", userCounter, " ", compCounter);
-        
-        let addIcon = document.getElementById(4);
-        
-        console.log("addIcon", addIcon);
-        
-        userCounter = 0;
-        compCounter = 0;
-        userScore.innerText = userCounter; 
-        compScore.innerText = compCounter; 
-
-
-        addIcon.addEventListener("click", function() {       
-                         
-            userChoice = addIcon.id;
-
-            // console.log("userChoice", userChoice);
-
-            compChoice = computerChoice();
-
-            result = calculateResult(userChoice, compChoice);
-
-            whoWon = showResult(userChoice, compChoice);             
-              
-                    
-        });
-        flagItems++;
-   
-    }   else if (level === 3 && flagItems === 3)   {
-
-            userCounter = 0;
-            compCounter = 0;
-            userScore.innerText = userCounter; 
-            compScore.innerText = compCounter;  
-    
-            console.log("RESET1", userCounter, " ", compCounter);
-            
-            let addIcon = document.getElementById(5);
-            
-            console.log("addIcon", addIcon);
-            
-            userCounter = 0;
-            compCounter = 0;
-            userScore.innerText = userCounter; 
-            compScore.innerText = compCounter; 
-    
-    
-            addIcon.addEventListener("click", function() {       
-                             
-                userChoice = addIcon.id;
-    
-                // console.log("userChoice", userChoice);
-    
-                compChoice = computerChoice();
-    
-                result = calculateResult(userChoice, compChoice);
-    
-                whoWon = showResult(userChoice, compChoice);             
-                  
-                        
-            }); 
-            flagItems++;       
-        }   else {
-            console.log("gameOver3");
-            gameOver();
-    }        
-    
-        
-}
-
-*/
-
 function menuItemClick() {               
                 
     userChoice = this.id;
@@ -443,13 +337,16 @@ function closePopup(){
 function gameOver() { 
     levelUpHide.style.visibility = "hidden";
     popUpLevel.style.display = "block";
-    popupLevelMessage.innerHTML = "<h1>Game over!</h1>";      
+    popupLevelMessage.innerHTML = "<h1>Game over!</h1>";
+    button.id = 'circular-menu-item-hidden';       
 }
 
 function gameOverWon() {
     levelUpHide.style.visibility = "hidden";
     popUpLevel.style.display = "block";
-    popupLevelMessage.innerHTML = "<h1> <div>Congraduations!</div><div>You beat the computer!</div><div>Game over!<div></h1>"; 
+    popupLevelMessage.innerHTML = "<h1> <div>Congraduations!</div><div>You beat the computer!</div><div>Game over!<div></h1>";
+    button.id = 'circular-menu-item-hidden';
+    hideGameItems();
 }
 
 function hideGameItems() {
